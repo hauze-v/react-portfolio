a19a159ea3c8e37c9da81d6c1d3929e1119d48ca
Adam Hauze <hauze_v@protonmail.com>
2021-01-27 15:01:38 -0500

    Added all the screenshots for all the projects.

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,12 @@
+// Stying and Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
 const Project = ({ project }) => {
   return (
     <div className="Project">
       <h2>{project.name}</h2>
+      <img src={project.bgImage} alt="Main screenshot for project work." />
     </div>
   );
 };
--- src/data.js
+++ src/data.js
@@ -3,6 +3,21 @@ import { v4 as uuidv4 } from "uuid";
 import igniteMain from "./img/ignite-main.jpg";
 import igniteSS01 from "./img/ignite-ss01.jpg";
 import igniteSS02 from "./img/ignite-ss02.jpg";
+import musicMain from "./img/music-main.png";
+import musicSS01 from "./img/music-ss01.png";
+import musicSS02 from "./img/music-ss02.png";
+import quizMain from "./img/quiz-main.png";
+import quizSS01 from "./img/quiz-ss01.png";
+import quizSS02 from "./img/quiz-ss02.png";
+import ipsumMain from "./img/ipsum-main.jpg";
+import ipsumSS01 from "./img/ipsum-ss01.jpg";
+import ipsumSS02 from "./img/ipsum-ss02.jpg";
+import hangmanMain from "./img/hangman-main.png";
+import hangmanSS01 from "./img/hamgna-ss01.png";
+import hangmanSS02 from "./img/hangman-ss02.png";
+import captureMain from "./img/capture-main.png";
+import captureSS01 from "./img/capture-ss01.png";
+import captureSS02 from "./img/capture-ss02.jpg";
 
 function getProjects() {
   return [
@@ -11,48 +26,48 @@ function getProjects() {
       name: "React Music Player",
       caseStudy:
         "This project marks my introduction to React.js. In order to build what you see here I had to learn JSX, how to handle state, and how to pass props. After spending a lot of time with the fundamentals of vanilla JS, I appreciate the speed and ease at which React.js can render things on the screen. Writing Sass was also a fun challenge.",
-      bgImage: "/",
-      screenshots: ["/", "/"],
+      bgImage: musicMain,
+      screenshots: [musicSS01, musicSS02],
     },
     {
       id: uuidv4(),
       name: "Quiz App",
       caseStudy:
         "Object oriented vanilla JavaScript quiz app built using the revealing module pattern. This project allowed me to apply my OOP knowledge through constructor functions and object prototypes.",
-      bgImage: "/",
-      screenshots: ["/", "/"],
+      bgImage: quizMain,
+      screenshots: [quizSS01, quizSS02],
     },
     {
       id: uuidv4(),
       name: "Final Fantasy Ipsum Generator",
       caseStudy:
         "Ever since I first used a custom-built ipsum generator like bacon ipsum back in college, I've always wanted to make one of my own. Since I'm a big nerd and couldn't find an ipsum generator for my favorite video game series, I decided this was the perfect project to practice my JavaScript skills with.",
-      bgImage: "/",
-      screenshots: ["/", "/"],
+      bgImage: ipsumMain,
+      screenshots: [ipsumSS01, ipsumSS02],
     },
     {
       id: uuidv4(),
       name: "React Videography Portfolio",
       caseStudy:
         "Prompto blizzard magic pot princess guard mega-phoenix dispel light robe Raiden dark knight. Cinna slowga adamantoise lion heart elixir haste ninja gear Cait Sith white mage. Relm slowga adamantoise lion heart sleeping bag blizzara myrthril vest Carbuncle samurai. Garnet thunder judges tiger racket echo screen fira rubber suit Kujata warrior.",
-      bgImage: "/",
-      screenshots: ["/", "/"],
+      bgImage: captureMain,
+      screenshots: [captureSS01, captureSS02],
     },
     {
       id: uuidv4(),
       name: "React Gaming Website",
       caseStudy:
         "Prompto blizzard magic pot princess guard mega-phoenix dispel light robe Raiden dark knight. Cinna slowga adamantoise lion heart elixir haste ninja gear Cait Sith white mage. Relm slowga adamantoise lion heart sleeping bag blizzara myrthril vest Carbuncle samurai. Garnet thunder judges tiger racket echo screen fira rubber suit Kujata warrior.",
-      bgImage: "/",
-      screenshots: ["/", "/"],
+      bgImage: igniteMain,
+      screenshots: [igniteSS01, igniteSS02],
     },
     {
       id: uuidv4(),
       name: "Hangman Game",
       caseStudy:
         "Prompto blizzard magic pot princess guard mega-phoenix dispel light robe Raiden dark knight. Cinna slowga adamantoise lion heart elixir haste ninja gear Cait Sith white mage. Relm slowga adamantoise lion heart sleeping bag blizzara myrthril vest Carbuncle samurai. Garnet thunder judges tiger racket echo screen fira rubber suit Kujata warrior.",
-      bgImage: "/",
-      screenshots: ["/", "/"],
+      bgImage: hangmanMain,
+      screenshots: [hangmanSS01, hangmanSS02],
     },
   ];
 }
