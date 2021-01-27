4e6de94961477b88e4f93da72b08f71ba06a0302
Adam Hauze <hauze_v@protonmail.com>
2021-01-27 15:42:09 -0500

    Got the project cards setup.

--- src/components/MyWorkSection.js
+++ src/components/MyWorkSection.js
@@ -1,21 +1,50 @@
 // Import Componenets
 import Project from "../components/Project";
+// Stying and Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const MyWorkSection = ({ projects }) => {
-  // Convert projects to an array
-  // const projectsArray = Object.keys(projects);
-
   return (
-    <div className="work-section">
-      <h2>My Work</h2>
-      <p>Here are a few of my recent projects</p>
-      <div className="projects">
+    <WorkSection>
+      <Header>
+        <h2>My Work</h2>
+        <p>Here are a few of my recent projects</p>
+      </Header>
+      <Projects>
         {projects.map((project) => (
           <Project project={project} key={project.id} />
         ))}
-      </div>
-    </div>
+      </Projects>
+    </WorkSection>
   );
 };
+
+const WorkSection = styled(motion.div)`
+  border: 2px solid blue;
+  padding: 0rem 5rem;
+`;
+
+const Header = styled(motion.div)`
+  h2 {
+    text-align: center;
+    margin-bottom: 1rem;
+  }
+  p {
+    text-align: center;
+    margin-bottom: 3rem;
+  }
+`;
+
+const Projects = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+  h2 {
+    padding: 2rem 0rem;
+  }
+`;
 
 export default MyWorkSection;
--- src/components/Project.js
+++ src/components/Project.js
@@ -4,11 +4,24 @@ import { motion } from "framer-motion";
 
 const Project = ({ project }) => {
   return (
-    <div className="Project">
+    <StyledProject>
       <h2>{project.name}</h2>
       <img src={project.bgImage} alt="Main screenshot for project work." />
-    </div>
+    </StyledProject>
   );
 };
+
+const StyledProject = styled(motion.div)`
+  min-height: 30vh;
+  box-shadow: 0px 5px 30px rgba(0, 0, 0, 0.2);
+  text-align: center;
+  border-radius: 1rem;
+  img {
+    height: 40vh;
+    object-fit: cover;
+    display: block;
+  }
+  overflow: hidden;
+`;
 
 export default Project;
